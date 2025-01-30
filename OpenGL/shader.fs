1e5370177f7bfcbb11e622b04cebac02557f8b49
Yogesh Rawat <rawatyogesh39@gmail.com>
2025-01-30 17:59:41 +0530

    Vertex coordinates are transformed via the model, view and projection matrix
    -Tilted backwards to the floor.
    -A bit farther away from us.
    -Be displayed with perspective (it should get smaller, the further its vertices are).

--- OpenGL/shader.fs
+++ OpenGL/shader.fs
@@ -10,6 +10,6 @@ uniform sampler2D texture2;
 
 void main()
 {
-    //FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    //FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);   // we were coloring here 
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * vec4(ourColor, 1.0);
 }
