7470887ddf0aa9a4995beae866ad81ab6df4a148
Yogesh Rawat <rawatyogesh39@gmail.com>
2025-01-30 15:42:05 +0530

    Added GLM library (OpenGL mathematic library)
    - Updated scripts to rotate and Translate the image at bottom right corner.

--- OpenGL/Source.cpp
+++ OpenGL/Source.cpp
@@ -5,6 +5,10 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "Util/stb_image.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 using namespace std;
 
 void processInput(GLFWwindow* window);
@@ -175,6 +179,16 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		// create transformations
+		glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		// get matrix's uniform location and set matrix
+		ourShader.use();
+		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
